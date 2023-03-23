3c951241b2b5036d523609408f8e9223bfed6686
AhmedSh4 <AhmedSh4>
2023-03-23 15:14:15 -0700

    action number 276

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-
 #include "main.h"
 #include <stdio.h>
 
 /**
 * main - Entry point
-*
 * Description: Prints numbers 1 to 100 but with
 * Fizz instead of multiples of 3 and Buzz instead
 * of multiples of 5. For multiples of both 3 and
