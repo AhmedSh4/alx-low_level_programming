ced5ec4511c36924bc7acf7e3ce72c97067ac4aa
AhmedSh4 <AhmedSh4>
2023-04-09 23:46:45 -0700

    im too late

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* rev_string - Reverses a string
+*
+* @s: The string to be reversed
+*/
+void rev_string(char *s)
+{
+int i, j;
+char temp;
+
+for (j = 0; s[j] != '\0'; j++)
+;
+
+j--;
+
+for (i = 0; i < j; i++, j--)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+}
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int *n);
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -13,4 +16,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif /* MAIN_H */
+#endif
