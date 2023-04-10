32ae96ee120dd5414f3e58afccb761a5898f7269
AhmedSh4 <125518743+AhmedSh4@users.noreply.github.com>
2023-04-10 20:59:38 +0200

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,6 +7,7 @@
 *
 * Return: 1 if the string is a palindrome, 0 otherwise
 */
+
 int is_palindrome(char *s)
 {
 int len = strlen(s);
@@ -14,9 +15,8 @@ int len = strlen(s);
 if (len <= 1)
 return (1);
 
-if (s[0] == s[len - 1])
-return (is_palindrome(s + 1) && is_palindrome(s, len - 2));
-
+if (*s != s[len - 1])
 return (0);
-}
 
+return (is_palindrome(s + 1, len - 2));
+}
