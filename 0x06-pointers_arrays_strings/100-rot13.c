8e1112a0d496f7f571345e148bdcb3ae358cb47e
AhmedSh4 <125518743+AhmedSh4@users.noreply.github.com>
2023-04-10 10:45:42 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,40 +1,34 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* rot13 - encodes a string using rot13
-* @s: the string to encode
+* rot13 - encode a string using the rot13 algorithm
+* @s: pointer to the string to encode
 *
-* Return: the encoded string
+* Return: pointer to the encoded string, or NULL if s is NULL
 */
 char *rot13(char *s)
 {
-char *p = s;
-char *alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+if (s == NULL)
+return (NULL);
 
-while (*s)
-{
-char *temp = alphabets;
-char *temp2 = rot13;
-int found = 0;
+char *encoded = malloc(sizeof(char) * (strlen(s) + 1));
+if (encoded == NULL)
+return (NULL);
 
-while (*temp)
+const char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const char *rot   = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (int i = 0; s[i] != '\0'; i++)
 {
-if (*temp == *s)
-{
-*s = *temp2;
-found = 1;
-break;
-}
-temp++;
-temp2++;
-}
-if (found)
-s++;
+char *p = strchr(alpha, s[i]);
+if (p == NULL)
+encoded[i] = s[i];
 else
-s++;
-}
-return (p);
+encoded[i] = rot[p - alpha];
 }
 
+encoded[strlen(s)] = '\0';
+return (encoded);
+}
