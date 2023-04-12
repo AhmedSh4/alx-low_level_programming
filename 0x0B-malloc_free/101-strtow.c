9e64465d2bd4f799c6e38a7a3f6ae6e42e8426df
AhmedSh4 <125518743+AhmedSh4@users.noreply.github.com>
2023-04-12 04:02:06 +0200

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,6 +12,9 @@ int count_words(char *str)
 {
 int i, count = 0;
 
+if (str == NULL)
+return (0);
+
 for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
