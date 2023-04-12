452e7f53962d664e76390b28ec8c3fe920e773f5
AhmedSh4 <125518743+AhmedSh4@users.noreply.github.com>
2023-04-12 03:41:18 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,7 +8,7 @@
 * Return: 0
 */
 
-char *my_strdup(char *src)
+char *_strdup(char *src)
 {
 char *dest;
 int i, len = 0;
@@ -21,8 +21,10 @@ dest = malloc(sizeof(char) * (len + 1));
 if (dest == NULL)
 return (NULL);
 
-for (i = 0; i <= len; i++)
+for (i = 0; i < len; i++)
 dest[i] = src[i];
+
+dest[i] = '\0';
 
 return (dest);
 }
