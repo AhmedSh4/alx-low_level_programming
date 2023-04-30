87864fc0450fa7f30d50c40d3a21e7f20fa4b808
AhmedSh4 <AhmedSh4>
2023-04-30 02:49:59 -0700

    i missed betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+* main - prints the name of the file it was compiled from
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("Hello, Holberton\n");
+printf("%s\n", __FILE__);
 return (0);
 }
 
